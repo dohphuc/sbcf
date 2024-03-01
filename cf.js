7ff4fcb4b3c1b394415f0e682a09433232cc10d5
notme <151835657+dohphuc@users.noreply.github.com>
2024-03-01 21:33:15 +0700

    Create cf.js

---
+++ cf.js
@@ -0,0 +1,23 @@
+[
+    {
+        "title": "\nCat Food + 5000",
+        "createdAt": 1709035200,
+        "items": [
+            {"itemId": 22, "itemCategory": 0, "amount": 5000, "title": "Cat Food"}
+        ]
+    },
+    {
+        "title": "\nCat Food + 10000",
+        "createdAt": 1709035200,
+        "items": [
+            {"itemId": 22, "itemCategory": 0, "amount": 10000, "title": "Cat Food"}
+        ]
+    },
+    {
+        "title": "\nCat Food + 20000",
+        "createdAt": 1709035200,
+        "items": [
+            {"itemId": 22, "itemCategory": 0, "amount": 20000, "title": "Cat Food"}
+        ]
+    }
+]
